928914e24c38763b969309b14a8a2408ea017bbe
Carlos Frias <carlosefrias@gmail.com>
2019-09-27 21:02:15 +0100

    multiple countdown windows

--- src/main.js
+++ src/main.js
@@ -6,24 +6,31 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain
 
-let mainWindow
+let windows = []
 
 app.on('ready', _ => {
-    mainWindow = new BrowserWindow({
-        height: 400,
-        width: 400
-    })
+    [1,2,3].forEach(_=>{      
+        let win = new BrowserWindow({
+            height: 400,
+            width: 400
+        })
 
-    mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+        win.loadURL(`file://${__dirname}/countdown.html`)
 
-    mainWindow.on('closed', _=>{
-        console.log('closed!')
-        mainWindow = null
+        win.on('closed', _=>{
+            console.log('closed!')
+            mainWindow = null
+        })
+
+        windows.push(win)
     })
 })
 
 ipc.on('countdown-start', _=>{
     countdown(count => {
-        mainWindow.webContents.send('countdown', count)
+        console.log('count', count)
+        windows.forEach(win =>{
+            win.webContents.send('countdown', count)
+        })
     })
 })
